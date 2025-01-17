7f670a74a81fe6beaaefee16bafb92c529670e0d
Patrick Minda <patrick.minda@hotmail.com>
2025-01-18 09:50:17 +1100

    scripts setup

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployRaffle is Script {
+
+    function run() public {
+
+    }
+
+    function deployContract() public returns(Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        // local => deploy mocks with local config
+        // sepolia => get sepolia config
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle({
+            config.entranceFee,
+            config.interval,
+            config.subscriptionId,
+            config.gasLane,
+            config.callbackGasLimit,
+
+        });
+        vm.stopBroadcast();
+
+        return (raffle, helperConfig);
+    }
+
+}
